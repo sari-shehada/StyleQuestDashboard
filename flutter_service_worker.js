504525981dfe817fb77e6489baaa7991ae9854cb
sari-shehada <sari-shehada@users.noreply.github.com>
2025-06-17 19:42:35 +0000

    deploy: sari-shehada/style-quest-dashboard@cc3d8cd8c8c885b40f4ee6ee05c258c24f642179

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -47,10 +47,10 @@ const RESOURCES = {"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "manifest.json": "d37c4da88379fe0a558eae1a785147b2",
-"index.html": "f911923a01cb3615faf66db74ede4934",
-"/": "f911923a01cb3615faf66db74ede4934",
+"index.html": "13204df1542a5e425817c0e76701f8d8",
+"/": "13204df1542a5e425817c0e76701f8d8",
 "version.json": "191595f72c94967ff44fc676a3aedb8c",
-"flutter_bootstrap.js": "5e4c1a9093b9d621ad858e17939e40a8",
+"flutter_bootstrap.js": "74dce159c62f4b5e6360cd34201bb123",
 "main.dart.js": "fe3b442157b86bef7a32e2c40624f64a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796"};
 // The application shell files that are downloaded before a service worker can
